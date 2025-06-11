41b34dd94a75ebab9c423373a8765104dd7d66b1
Ауд-Нагатинская-817 Студент <edu-msk22-817s@synergy.ru>
2025-06-11 12:11:16 +0300

    Change in file ProgressBar.jsx

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '/ProgressBar.css' //Для стилей
+
+function ProgressBar() {
+    return(
+        <div>
+            { //Создам скруглённый прямоугольник
+
+            }
+        </div>
+    )
+}
+
+export default ProgressBar
