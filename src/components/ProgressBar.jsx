04e05ca2a3868cff6367d547fbad06f1f418be8c
Tujh-2007 <tikegor23@gmail.com>
2025-06-17 07:36:38 +0300

    Update ProgressBar.jsx

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import './style/ProgressBar.css' //Для стилей
+import '../style/ProgressBar.css' //Для стилей
 
-function ProgressBar (task) {
+export const ProgressBar = (task) => {
     //Значения по умолчанию
-    let completed = task.completed ?? 0
-    let total = task.total ?? 1
+    const completed = task.completed ?? 65
+    const total = task.total ?? 100
      
     //Нахождение процента выполнения
     const procent = total > 0 ? Math.round ((completed / total)*100) : 0
 
     const ProgressColor = () => {
         if (procent < 50) return '#ff0000'
-        if (procent < 69) return '#ffa500'   
-        if (procent > 70) return '#00ff00'
+        if (procent <= 69) return '#ffa500'   
+        if (procent >= 70) return '#00ff00'
     }
     return(
         /*Контейнер для компонента*/
@@ -25,11 +25,10 @@ function ProgressBar (task) {
          <div className='ProgressBarRectangle'>
          {/*Заполненная часть шкалы прогресса*/}
           <div className='ProgressBarColor'
-          style={{
-             width: '${procent}%',
-             backgroundColor: ProgressColor()
-          }
-          }>
+           style={{
+              width: procent*1.5,
+              backgroundColor: ProgressColor()
+          }}>
           </div>            
          </div>
 
@@ -40,5 +39,3 @@ function ProgressBar (task) {
         </div> 
     )
 }
-
-export default ProgressBar
