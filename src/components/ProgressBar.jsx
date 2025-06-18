4e1fab0b9284c8705b321f079d2081549291b611
Tujh-2007 <tikegor23@gmail.com>
2025-06-18 11:23:39 +0300

    Update ProgressBar.jsx

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -3,17 +3,11 @@ import '../style/ProgressBar.css' //Для стилей
 
 export const ProgressBar = (task) => {
     //Значения по умолчанию
-    const completed = task.completed ?? 2
+    const completed = task.completed ?? 8
     const total = task.total ?? 10
      
     //Нахождение процента выполнения
     const procent = total > 0 ? Math.round((completed / total)*100) : 0
-
-    const ProgressColor = () => {
-        if (procent < 50) return '#FF3042'
-        if (procent <= 69) return '#FF9F43'   
-        if (procent >= 70) return '#21C971'
-    }
     
     return(
         /*Контейнер для компонента*/
@@ -28,7 +22,6 @@ export const ProgressBar = (task) => {
           <div className='ProgressBarColor'
            style={{
               width: procent*3.4,
-              backgroundColor: ProgressColor(),
               filter: 'blur(1.4px)',             
           }}>
           </div>            
